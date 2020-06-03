803a81d0deca7f17c6ba846a51a5142566a34ff6
osborne7 <eleanor.c.osborne@gmail.com>
2020-06-03 14:53:34 -0400

    Add onChange event to input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 //functional App component:
-const App = (props) => {
-return <h1>Hi, I'm {props.name}!</h1>
+// const App = (props) => {
+// return <h1>Hi, I'm {props.name}!</h1>
+// }
+
+//class-based App component:
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: "James"
+        };
+    }
+
+    handleInputChange = (text) => {
+        this.setState({text});
+    }
+    render() {
+    // return <h1>Hi, I'm {this.props.name} and this is {this.state.text}!</h1>
+    return (
+        <div>
+            <input value={this.state.text}
+            onChange={ (event) => { this.handleInputChange(event.target.value)}} />
+        </div>
+    )
+
+    }
 }
 
 export default App;
